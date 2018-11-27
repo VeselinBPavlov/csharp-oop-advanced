e7003f59b2ec422d2ad37b76f06751667277b734
VeselinBPavlov <veselin.b.pavlov@gmail.com>
2018-11-27 14:36:47 +0200

    Added exercise Iterators and Comparators.

---
+++ 06. Iterators and Comparators - Exercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,55 @@
+﻿namespace ListyIterator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ListyIterator<T>
+    {
+        private IList<T> items;
+        private int index;
+
+        public ListyIterator()
+        {
+            this.items = new List<T>();
+            this.index = 0;
+        }
+
+        public void Create(T[] items)
+        {
+            foreach (var item in items)
+            {
+                this.items.Add(item);
+            }
+        }
+
+        public bool Move()
+        {
+            if (this.index == this.items.Count - 1)
+            {
+                return false;
+            }
+            this.index++;
+            return true;
+        }
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.items[this.index]);
+        }
+
+        public bool HasNext()
+        {
+            if (this.index == this.items.Count - 1)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ 06. Iterators and Comparators - Exercise/ListyIterator/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace ListyIterator
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            ListyIterator<string> listyIterator = new ListyIterator<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                try
+                {
+                    var data = command.Split();
+
+                    switch (data[0])
+                    {
+                        case "Create": listyIterator.Create(data.Skip(1).ToArray()); break;
+                        case "Move": Console.WriteLine(listyIterator.Move()); break;
+                        case "HasNext": Console.WriteLine(listyIterator.HasNext()); break;
+                        case "Print": listyIterator.Print(); break;
+                        default: break;
+                    }
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    Console.WriteLine(ioe.Message);
+                }                
+            }
+        }
+    }
+}
